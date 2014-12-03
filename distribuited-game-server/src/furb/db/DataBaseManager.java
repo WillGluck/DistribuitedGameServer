67fee8a19ec5e931b1cd3e097d5654b3f432a444
Will Glück <bigwillgluck@gmail.com>
2014-12-02 22:07:00 -0200

    Mais um

--- distribuited-game-server/src/furb/db/DataBaseManager.java
+++ distribuited-game-server/src/furb/db/DataBaseManager.java
@@ -27,6 +27,28 @@ public class DataBaseManager {
 		this.initDataBase();
 	}
 	
+/*	public static void main(String[] args) {
+		
+		Player player = new Player();
+		player.name = "caique";
+		player.last_saved = new Date().getTime();
+		player.life = 90;
+		List<Integer> positions = new ArrayList<Integer>();
+		positions.add(30);
+		positions.add(20);
+		player.position = positions;
+		
+		DataBaseManager.getInstance().insertPlayer(player);
+		Player playerOne = DataBaseManager.getInstance().getPlayer("william");
+		Player playerTwo = DataBaseManager.getInstance().getPlayer("caique");
+		
+		playerTwo.life = 150;
+		
+		DataBaseManager.getInstance().updatePlayer(playerTwo);
+		playerTwo = DataBaseManager.getInstance().getPlayer("caique");
+		
+	}*/
+	
 	public static DataBaseManager getInstance() {
 		if (DataBaseManager.instance == null) {
 			DataBaseManager.instance = new DataBaseManager();
@@ -37,12 +59,13 @@ public class DataBaseManager {
 	public Player getPlayer(String userName) {
 		Player player = null;
 		try {
-			Statement statement = this.db.createStatement();			
-			ResultSet result = statement.executeQuery("select * from player where " + userName + " = " + userName + ";");
+			Statement statement = this.db.createStatement();	
+			String sql = "select * from player where " + USERNAME + " = '" + userName + "';";
+			ResultSet result = statement.executeQuery(sql);
 			while (result.next()) {
 				player = new Player();
 				player.name = result.getString(USERNAME);
-				player.last_saved = result.getLong(LAST_SAVED);
+				player.last_saved = result.getTimestamp(LAST_SAVED).getTime();
 				player.life = result.getInt(LIFE_POINTS);
 				int positionX = result.getInt(POSITION_X);
 				int positionY = result.getInt(POSITION_Y);
@@ -60,8 +83,8 @@ public class DataBaseManager {
 	public void updatePlayer(Player player) {
 		try {
 			Statement statement = this.db.createStatement();
-			String sql = "update player set " + LAST_SAVED 	+ " = " + new Date().getTime() + "," + LIFE_POINTS + " = " + player.life + ","
-					+ POSITION_X 	+ " = " + player.position.get(0) + "," + POSITION_Y 	+ " = " + player.position.get(1) + "where " + USERNAME + "=" + player.name + ";"; 
+			String sql = "update player set " + LAST_SAVED 	+ " = to_timestamp(" + new Date().getTime() + ")," + LIFE_POINTS + " = " + player.life + ","
+					+ POSITION_X 	+ " = " + player.position.get(0) + "," + POSITION_Y 	+ " = " + player.position.get(1) + "where " + USERNAME + "='" + player.name + "';"; 
 											  
 			statement.execute(sql);			
 		} catch (SQLException sqle) {
@@ -72,7 +95,8 @@ public class DataBaseManager {
 	public void insertPlayer(Player player) {
 		try {
 			Statement statement = this.db.createStatement();
-			String sql = "insert into player values ('" + player.name + "'," + player.life + "," + player.position.get(0) +	"," + player.position.get(1) + "," + new Date().getTime();											  
+			String sql = "insert into player values ('" + player.name + "'," + player.life + "," + player.position.get(0) +	"," + player.position.get(1) + "," 
+					+ "to_timestamp(" + new Date().getTime() + "));";											  
 			statement.execute(sql);			
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
@@ -83,7 +107,7 @@ public class DataBaseManager {
 	private void initDataBase() {
 		try {
 			Class.forName("org.postgresql.Driver");
-			this.db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgre", "postgres", "pfafveiou");
+			this.db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "pfafveiou");
 			this.createTables();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
